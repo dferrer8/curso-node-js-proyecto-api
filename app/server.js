4ccf043c8218013ff31607efc44c9d3fd422f58f
dferrer8 <david.dfg@gmail.com>
2018-10-03 16:49:59 +0200

    Commit inicial y añadidas rutas

--- app/server.js
+++ app/server.js
@@ -11,6 +11,19 @@ app.get('/cervezas', (req, res) => {
   res.json({ mensaje: '¡A beber cerveza!' })
 })
 
+app.post('/cervezas', (req, res) => {
+  res.json({ mensaje: '¡Cerveza añadida!' })
+})
+
+app.delete('/cervezas', (req, res) => {
+  res.json({ mensaje: '¡Cerveza borrada!' })
+})
+
 // iniciamos nuestro servidor
-app.listen(port)
-console.log('API escuchando en el puerto ' + port)
+// app.listen(port)
+// console.log('API escuchando en el puerto ' + port)
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`) // lo usamos para cuando si que se haya levantado el servidor
+})
+
+console.log('API todavía no escuchando!!!')
