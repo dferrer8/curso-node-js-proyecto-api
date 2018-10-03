9ed308b1c8376382df5308cc998511f4ed4f146c
dferrer8 <david.dfg@gmail.com>
2018-10-03 17:01:36 +0200

    Añadido el enroutador y primeras rutas con api

--- app/server.js
+++ app/server.js
@@ -3,20 +3,31 @@ var app = express()
 
 var port = process.env.PORT || 8080 // establecemos nuestro puerto
 
-app.get('/', (req, res) => {
-  res.json({ mensaje: '¡Hola Mundo!' })
+// para establecer las distintas rutas, necesitamos instanciar el express router
+var router = express.Router()
+
+// establecemos nuestra primera ruta, mediante get.
+router.get('/', (req, res) => {
+  res.json({ mensaje: '¡Bienvenido a nuestra API!' })
 })
 
-app.get('/cervezas', (req, res) => {
+router.get('/cervezas', (req, res) => {
   res.json({ mensaje: '¡A beber cerveza!' })
 })
 
-app.post('/cervezas', (req, res) => {
+router.post('/cervezas', (req, res) => {
   res.json({ mensaje: '¡Cerveza añadida!' })
 })
 
-app.delete('/cervezas', (req, res) => {
+router.delete('/cervezas', (req, res) => {
   res.json({ mensaje: '¡Cerveza borrada!' })
+})
+// nuestra ruta irá en http://localhost:8080/api
+// es bueno que haya un prefijo, sobre todo por el tema de versiones de la API
+app.use('/api', router)
+
+app.get('/', (req, res) => {
+  res.json({ mensaje: '¡Hola Mundo!' })
 })
 
 // iniciamos nuestro servidor
